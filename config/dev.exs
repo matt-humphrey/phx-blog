3012732cdbd88bf041ca9ffd6fcb3927a1c7cbe1
Matt Humphrey <mhumphrey9@hotmail.com>
2023-04-04 10:43:43 +0800

    install tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :blog, BlogWeb.Endpoint,
   secret_key_base: "rpg0zcYZoSlKMuKhWg45k4zXdf8CHHX1q/7ElniHe4UgFt2P+7XUq0rij0Y/OSKs",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
