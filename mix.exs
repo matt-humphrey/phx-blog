edb7721e70bef9b82b810fe4ada98817717ae4d7
Matt Humphrey <mhumphrey9@hotmail.com>
2023-05-09 12:05:08 +0800

    add credo and dialyxir

--- mix.exs
+++ mix.exs
@@ -50,7 +50,9 @@ defmodule Blog.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
-      {:faker, "~> 0.17"}
+      {:faker, "~> 0.17"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: :dev, runtime: false}
     ]
   end
 
